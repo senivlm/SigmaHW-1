e1360764db2d8185332dcd5857d3ffcb8c5717d0
Novak Oleg <novakoleg107@gmail.com>
2022-06-24 12:22:07 +0300

    Task 11 has been completed

---
+++ Task11/MyList.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task11
+{
+    internal class MyList<T> : IList<T>, IComparer<T>
+    {
+        private List<T> myArrayList;
+
+        public MyList()
+        {
+            myArrayList = new List<T>();
+        }
+
+        public MyList(T[] item)
+        {
+            if (item == null) throw new NullReferenceException();
+            myArrayList = new List<T>();
+
+            for (int i = 0; i < item.Length; i++)
+            {
+                myArrayList.Add(item[i]);
+            }
+        }
+
+        public T this[int index]
+        { 
+            get => myArrayList[index];
+            set
+            {
+                if(value == null || index < 0 || index > Count - 1)
+                {
+                    throw new ArgumentException();
+                }
+                myArrayList[index] = value;
+            }
+        }
+
+        public int Count => myArrayList.Count;
+
+        public bool IsReadOnly => false;
+
+        public void Add(T item)
+        {
+            myArrayList.Add(item);
+        }
+
+        public void Sort()
+        {        
+            myArrayList.Sort();
+        }
+
+        public void Sort(Comparer<T> comparer, int index = 0)
+        {
+            myArrayList.Sort(0, Count, comparer);
+        }
+
+        public void Clear()
+        {
+            myArrayList.Clear();
+        }
+
+        public bool Contains(T item) => myArrayList.Contains(item);
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null) throw new ArgumentNullException();
+            
+            if(arrayIndex < 0 & arrayIndex > array.Length) throw new ArgumentOutOfRangeException();
+
+            for (int i = arrayIndex; i < array.Length; i++)
+            {
+                myArrayList.Add(array[i]);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return myArrayList[i];
+            }
+        }
+
+        public int IndexOf(T item)
+        {
+            return myArrayList.IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (myArrayList.Count > index & index >= 0)
+            {
+                myArrayList.Insert(index, item);
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+        }
+
+        public bool Remove(T item)
+        {
+            if (myArrayList.Contains(item))
+            {
+                return myArrayList.Remove(item);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (myArrayList.Count > index & index >= 0)
+            {
+                myArrayList.RemoveAt(index);
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            string result = default;
+            foreach (var item in myArrayList)
+            {
+                result += item + " ";
+            }
+            return result;
+        }
+
+        private int FindIndex(int left, int right, T item)
+        {
+            if(right - left == 1)
+            {
+                return myArrayList[left].Equals(item) ? left : -1;
+            }
+
+            int middle = (left + right) / 2;
+
+            if (Compare(item, myArrayList[middle]) == 0)
+            {
+                return middle;
+            }
+
+            if (Compare(myArrayList[middle], item) < 0)
+            {
+                right = middle;
+            }
+            else
+            {
+                left = middle;
+            } 
+            
+            return FindIndex(left, right, item);
+        }
+
+        public int FindIndex(T item)
+        {
+            int left = 0;
+            int right = myArrayList.Count;
+
+            myArrayList.Sort();
+
+            return FindIndex(left, right, item);
+        }
+
+        public int Compare(T x, T y)
+        {
+            List<T> compareList = new List<T>();
+            compareList.Add(x);
+            compareList.Add(y);
+            compareList.Sort();
+
+            if (x.Equals(y))
+            {
+                return 0;
+            }
+            if (compareList[0].Equals(x))
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+    }
+}
---
+++ Task11/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //testClassMylistInt();
+            //testClassMylistString();
+            //testClassMylistObj();
+            testClassMylistMyClass();
+
+
+            Console.ReadKey();
+        }
+
+        public static void testClassMylistInt()
+        {
+            MyList<int> testList = new MyList<int>();
+
+            testList.Add(25);
+            testList.Add(2);
+            testList.Add(55);
+            testList.Add(14);
+
+            foreach (var item in testList)
+            {
+                Console.WriteLine(" " + item);
+            }
+            Console.WriteLine();
+
+            testList.Remove(2);
+            testList.Remove(1);
+
+            var comparer = Comparer<int>.Create((int x, int y)=> x.CompareTo(y));
+            testList.Sort(comparer);
+
+            foreach (var item in testList)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(testList.FindIndex(32));
+        }
+
+        public static void testClassMylistString()
+        {
+            MyList<string> testList = new MyList<string>();
+
+            testList.Add("dcc");
+            testList.Add("abs");
+            testList.Add("123");
+            testList.Add("end");
+
+            foreach (var item in testList)
+            {
+                Console.WriteLine(" " + item);
+            }
+            Console.WriteLine();
+
+            testList.Remove("f");
+
+            testList.Sort();
+            testList.CopyTo(new string[] { "sccs", "scscsc" }, 0);
+
+            var comparer = Comparer<string>.Create((string x, string y) => x.CompareTo(y));
+            testList.Sort(comparer);
+
+            foreach (var item in testList)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+            Console.WriteLine(testList.FindIndex("!"));
+
+        }
+
+        public static void testClassMylistMyClass()
+        {
+            MyList<MyList<int>> testList = new MyList<MyList<int>>();
+
+            testList.Add(new MyList<int>() {5,4,3,8});
+            testList.Add(new MyList<int>() { 55, 44, 33, 08 });
+
+            foreach (var item in testList)
+            {
+                Console.WriteLine(" " + item);
+            }
+            Console.WriteLine();
+
+            foreach (var item in testList)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(testList.FindIndex(new MyList<int>(){55,44,33,08}));
+
+        }
+
+        public static void testClassMylistObj()
+        {
+            MyList<object> testList = new MyList<object>();
+
+            testList.Add(1);
+            testList.Add("abs");
+            testList.Add(2.5m);
+            testList.Add(DateTime.Now);
+
+            foreach (var item in testList)
+            {
+                Console.WriteLine(" " + item);
+            }
+            Console.WriteLine();
+
+            foreach (var item in testList)
+            {
+                Console.Write(" " + item.GetType());
+            }
+            Console.WriteLine(testList.FindIndex(2.5m));
+
+        }
+    }
+}
